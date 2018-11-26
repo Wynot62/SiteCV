9b1134dcb971392592d4109b0534801fe712ebb8
Donovan <wynot1402@gmail.com>
2018-11-26 09:23:47 +0100

    Changement menu

---
+++ Javascript.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    setTimeout(
+        function(){
+            $('#paysage').animate({right:'0%'},1500);
+        },500);
+    setTimeout(
+        function(){
+            $( '#TextP').animate({left:'0%'},2000);
+        },500);
+    
+    
+});
