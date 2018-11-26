7c6f779337a3c2c4bf51c702a054b6478138b4cf
Donovan <wynot1402@gmail.com>
2018-11-26 11:47:14 +0100

    Début adaptation smartphone

--- Javascript.js
+++ Javascript.js
@@ -1,13 +1,36 @@
 $(document).ready(function(){
 
+    //Fond des pages//
     setTimeout(
         function(){
             $('#paysage').animate({right:'0%'},1500);
         },500);
+    //Text page1
     setTimeout(
         function(){
             $( '#TextP').animate({left:'0%'},2000);
         },500);
+    //text page2
+    setTimeout(
+        function(){
+            $('#TextC').animate({left:'0%'},2000);
+        },500);
+    //animation device page2
+    setTimeout(
+        function(){
+            $('#ecran').animate({top:'50%',opacity:'1'},2000);
+        },600);
+    setTimeout(
+        function(){
+            $('#tablette').animate({top:'55%',opacity:'1'},2000);
+        },700);
+    setTimeout(
+        function(){
+            $('#Phone').animate({top:'60%',opacity:'1'},2000);
+            
+        },800);
+    
+    
     
     
 });
